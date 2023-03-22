05c139b4e66b0b11b0afbc235353d70eec63ebdb
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2023-03-22 12:00:52 +0100

    Update DifShkKsh100.cpp

--- Exercise 006/DifShkKsh100.cpp
+++ Exercise 006/DifShkKsh100.cpp
@@ -1,8 +1,8 @@
 /*  --------------------------------------------------------------------
 	Shuma e katroreve te dhjete numrave te pare naturor eshte,
-    1^2 + 2^2 + ... + 10^2 = 385
-    Katrori i shumes se dhjete numrave te pare naturor eshte,
-    (1 + 2 + ... + 10)^2 = 552 = 3025
+        1^2 + 2^2 + ... + 10^2 = 385
+        Katrori i shumes se dhjete numrave te pare naturor eshte,
+        (1 + 2 + ... + 10)^2 = 552 = 3025
   	Nderkohe qe diferenca mes katrorit te shumes dhe shumes se katroreve
   	te dhjete numrave te pare natyror eshte 3025 - 385 = 2640.
   	Gjeni diferencen mes katrorit te shumes dhe shumes se katroreve 
