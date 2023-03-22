8cf9d85d982cb17184a9f11e758d11a8c013325a
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2023-03-22 12:06:48 +0100

    Update Prob007.cpp

--- Exercise 007/Prob007.cpp
+++ Exercise 007/Prob007.cpp
@@ -7,9 +7,9 @@
 #include <math.h>
 using namespace std;
 
-bool prim (int n)					// Funksion kontrollimi per numrin e rradhes
+bool prim (int n)				// Funksion kontrollimi per numrin e rradhes
 {
-    for (int i=2; i<=sqrt(n); i++)	// Kontrollojme per pjestues deri tek rrenja katrore e numrit
+    for (int i=2; i<=sqrt(n); i++)	        // Kontrollojme per pjestues deri tek rrenja katrore e numrit
          if ( n%i == 0)
             return 0;
     return 1;
@@ -20,9 +20,9 @@ int main()
     for (int i=2;  ; i++)			// Gjenerojme numrat me rradhe
      {   if ( prim (i) )			// Therrasim funksionin e kontrollimit te numrave prim
         	{
-				j++;				// Sa here gjendet nje nr prim numeratori j rritet me 1
+				j++;		// Sa here gjendet nje nr prim numeratori j rritet me 1
         	}
-	if ( j == 10001 )				// Kontrollojme vleren e numeratorit
+	if ( j == 10001 )			// Kontrollojme vleren e numeratorit
 			{
     			cout << "---------------------------" << endl;
 				cout << "Numri PRIM i 10001-ti eshte" << endl;
