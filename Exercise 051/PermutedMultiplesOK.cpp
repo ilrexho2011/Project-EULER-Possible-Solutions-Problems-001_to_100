9d46b536a53dcb840507b4976294afaaf1c30abb
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2023-04-01 19:25:13 +0200

    Update PermutedMultiplesOK.cpp

--- Exercise 051/PermutedMultiplesOK.cpp
+++ Exercise 051/PermutedMultiplesOK.cpp
@@ -1,5 +1,5 @@
 //	Permuted multiples
-/*	It can be seen that the number, 125874, and its double, 251748, 
+/* It can be seen that the number, 125874, and its double, 251748, 
 contain exactly the same digits, but in a different order.
 
 Find the smallest positive integer, x, 
