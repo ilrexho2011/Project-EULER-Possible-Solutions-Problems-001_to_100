bd714ba0961d497262d8da956035dea790805469
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2023-04-09 14:38:25 +0200

    Add files via upload

---
+++ Exercise 020/Exercise 020.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Problem: n! means n × (n − 1) × ... × 3 × 2 × 1
+ *
+ * For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+ * and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+ *
+ * Find the sum of the digits in the number 100!
+ *
+ * Solution:
+ */
+
+include "helper.php";
+
+function factorial($num)
+{
+	$tot = 1;
+	for ($i=1; $i <= $num ; $i++) {
+		$tot *= $i;
+	}
+	$tot = sprintf('%f', $tot); //Convert from Scientific notation
+	$tot = substr($tot, 0, -7); //Remove the trailing decimal places
+	return $tot;
+}
+
+$fact = factorial(100);
+$fact = str_split($fact);
+$total = 0;
+foreach ($fact as $digit) {
+	$total += $digit;
+}
+echo "Project Euler lies.  The answer really is 683.\n";
+result(648, $total);
