fc0e30401517620b2b148276003ee4f21e0815b1
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2023-04-09 14:32:26 +0200

    Add files via upload

--- Exercise 016/Exercise 016.php
+++ Exercise 016/Exercise 016.php
@@ -1,12 +1,18 @@
 <?php
-$number = bcpow('2','1000');
-$length = strlen($number);
-$j = 0;
+/**
+ * Problem: 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ *
+ * What is the sum of the digits of the number 2^1000?
+ *
+ * Solution: The trick is the scientific notation.  First, we generate the 2^1000
+ * number.  Then I'll strip all the trailing zero digits since we don't need them.
+ * Finally, sum the rest of the numbers.
+ */
 
-for ($i = 0; $i < $length; $i++)
-        {
-         $j += $number[$i];
-}
-echo $j;
+include "helper.php";
 
-?>
+$num = sprintf('%f', pow(2, 1000));
+$num = str_split(substr($num, 0, -7));
+$tot = array_sum($num);
+
+result(1366, $tot);
